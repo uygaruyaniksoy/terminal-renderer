9aaab25861d85660ee38d2872788af9176d9e6c9
Uygar Uyanıksoy <uygaruyaniksoy@gmail.com>
2018-09-03 15:58:30 +0100

    fixed bug where lenna might be rendered twice and moved the rendering to the center of the terminal

--- index.js
+++ index.js
@@ -25,28 +25,7 @@ program.on('keypress', function(ch, key) {
     process.exit(0);
   }
 });
-/*
-program.on('mouse', function(data) {
-  if (data.action === 'mouseup') return;
-  program.move(1, program.rows);
-  program.eraseInLine('right');
-  if (data.action === 'wheelup') {
-    program.write('Mouse wheel up at: ' + data.x + ', ' + data.y);
-  } else if (data.action === 'wheeldown') {
-    program.write('Mouse wheel down at: ' + data.x + ', ' + data.y);
-  } else if (data.action === 'mousedown' && data.button === 'left') {
-    program.write('Left button down at: ' + data.x + ', ' + data.y);
-  } else if (data.action === 'mousedown' && data.button === 'right') {
-    program.write('Right button down at: ' + data.x + ', ' + data.y);
-  } else {
-    program.write('Mouse at: ' + data.x + ', ' + data.y);
-  }
-  program.move(data.x, data.y);
-  program.bg('red');
-  program.write(' ');
-  program.bg('!red');
-});
-*/
+
 program.alternateBuffer();
 program.hideCursor();
 program.clear();
@@ -69,62 +48,69 @@ const loop = function (options) {
     let imageAspectRatio = imageWidth / imageHeight;
 
     let output = [];
-    
+
     let imageRenderHeight;
     let imageRenderWidth;
     let horizontalRatio;
     let verticalRatio;
     let isHorizontal = false;
+    let leftOffset;
+    let topOffset;
     if (terminalAspectRatio > imageAspectRatio) {
       imageRenderHeight = height;
       imageRenderWidth = height * imageAspectRatio;
+      leftOffset = width / 2 - imageRenderWidth / 2;
     } else {
       imageRenderHeight = width / imageAspectRatio;
       imageRenderWidth = width;
       isHorizontal = true;
+      topOffset = height / 2 - imageRenderHeight / 2;
     }
     horizontalRatio = imageWidth / imageRenderWidth;
     verticalRatio = imageHeight / imageRenderHeight;
     for (let y = 0; y < height; y++) {
       for (let x = 0; x < width; x++) {
-        if (!isHorizontal && (x < (width / 4 - imageRenderWidth / 2) || x > (width / 4 + imageRenderWidth / 2))) {
-          output[y * height + x] = 0;
-        } else if (isHorizontal && (y < (height / 2 - imageRenderHeight / 2) || y > (height / 2 + imageRenderHeight / 2))) {
-          output[y * height + x] = 0;
+        if (!isHorizontal && (x < leftOffset || x > (width - leftOffset))) {
+          output[y * width + x] = 0;
+        } else if (isHorizontal && (y < topOffset || y > (height - topOffset))) {
+          output[y * width + x] = 0;
         } else {
           let cellValue = 0;
           for (let j = 0; j < verticalRatio; j++) {
             for (let i = 0; i < horizontalRatio; i++) {
-              let color = image.getPixelColor((horizontalRatio * x | 0) + i, (verticalRatio * y | 0) + j);
+              let color;
+              if (isHorizontal) {
+                color = image.getPixelColor((horizontalRatio * x | 0) + i, (verticalRatio * (y - topOffset) | 0) + j);
+              } else {
+                color = image.getPixelColor((horizontalRatio * (x - leftOffset) | 0) + i, (verticalRatio * y | 0) + j);
+              }
               let r = (color >> 24) & 0xFF;
               let g = (color >> 16) & 0xFF;
               let b = (color >>  8) & 0xFF;
               let a = (color >>  0) & 0xFF;
               cellValue += (r + g + b) * (a / 255 / 3);
-            } 
+            }
           }
           cellValue = cellValue / ((horizontalRatio + 1) | 0) / ((verticalRatio + 1) | 0);
-          output[y * height + x] = cellValue >> 7;
-          if ((cellValue >> 7) > 1) console.log(cellValue >> 7);
+          output[y * width + x] = cellValue >> 7;
         }
       }
     }
 
-    program.move(0,0);
+    program.clear();
     for (let y = 0; y < height; y+=4) {
       for (let x = 0; x < width; x+=2) {
-        let unicodeValue = (output[y * height + x] || 0) +
-          ((output[(y + 1) * height + x] || 0) << 1) +
-          ((output[(y + 2) * height + x] || 0) << 2) +
-          ((output[(y + 3) * height + x] || 0) << 6) +
-          ((output[(y + 0) * height + x + 1] || 0) << 3) +
-          ((output[(y + 1) * height + x + 1] || 0) << 4) +
-          ((output[(y + 2) * height + x + 1] || 0) << 5) +
-          ((output[(y + 3) * height + x + 1] || 0) << 7);
+        program.move(x / 2, y / 4);
+        let unicodeValue = (output[y * width + x] || 0) +
+          ((output[(y + 1) * width + x] || 0) << 1) +
+          ((output[(y + 2) * width + x] || 0) << 2) +
+          ((output[(y + 3) * width + x] || 0) << 6) +
+          ((output[(y + 0) * width + x + 1] || 0) << 3) +
+          ((output[(y + 1) * width + x + 1] || 0) << 4) +
+          ((output[(y + 2) * width + x + 1] || 0) << 5) +
+          ((output[(y + 3) * width + x + 1] || 0) << 7);
         program.write(getUnicodeValue(unicodeValue));
-        // program.write(""+(x%10));
       }
-      if (y + 4 < height) program.feed();
     }
 
   }
@@ -136,27 +122,3 @@ Jimp.read('lenna.png', (err, lenna) => {
   if (err) throw err;
   image = lenna.greyscale();
 });
-
-/*
-
-program.move(1, 1);
-program.bg('black');
-program.write('Hello world', 'blue fg');
-program.setx((program.cols / 2 | 0) - 4);
-program.down(5);
-program.write('Hi again!');
-program.bg('!black');
-program.feed();
-program.write('Hi again!');
-
-program.getCursor(function(err, data) {
-  if (!err) {
-    program.write('Cursor is at: ' + data.x + ', ' + data.y + '.');
-    program.feed();
-  }
-
-  program.charset('SCLD');
-  program.write('abcdefghijklmnopqrstuvwxyz0123456789');
-  program.charset('US');
-  program.setx(1);
-});*/
